57fd6ff5b1bd75a594cfb781fb8d75fc3305bdfa
Arthur Barr <arthur.barr@uk.ibm.com>
2017-04-13 09:35:24 +0100

    Updates from linters

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,8 @@ maintainer_email 'arthur.barr@uk.ibm.com'
 license 'Apache 2.0'
 description 'Installs/Configures IBM MQ'
 long_description 'Installs/Configures IBM MQ'
+source_url 'https://github.com/ibm-messaging/mq-chef'
+issues_url 'https://github.com/ibm-messaging/mq-chef/issues'
 
 version '0.1.2'
 
@@ -11,3 +13,5 @@ supports 'ubuntu'
 
 depends 'limits'
 depends 'yum'
+
+chef_version '>= 12.6' if respond_to?(:chef_version)
